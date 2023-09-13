933de34f39cbbd40cfa232c6472a97cd89da805b
Sven Tomašek <61661787+sventomasek@users.noreply.github.com>
2023-09-14 00:06:34 +0200

    Update Camera.gd

--- Camera.gd
+++ Camera.gd
@@ -6,9 +6,10 @@ extends Camera2D
 @export var ignoreY = false
 @export var checkIfMouseInWindow = true
 
-@export var directionalOffset = Vector2(0, 0)
+@export var speed = Vector2(0.1, 0.25)
 @export var fixedOffset = Vector2(0, -15)
-@export var smoothness = Vector2(0.1, 0.25)
+@export var dirOffset = Vector2(0, 0)
+@export var dirOffsetSpeed = 0.05
 
 @export var mouseMoveAmount = Vector2(0.3, 0.3)
 @export var joystickMoveAmount = Vector2(40, 25)
@@ -18,8 +19,9 @@ var usingMouse = true
 var mouseInWindow = true
 var mousePosition = Vector2(0, 0)
 var joystickDirection = Vector2(0, 0)
-var currentDirOffset = Vector2(directionalOffset.x, 0)
+var currentDirOffset = Vector2(dirOffset.x, 0)
 var velocity = Vector2(0, 0)
+var facingRight = true
 
 func _physics_process(delta):
 	var myPosition = Vector2(0, 0)
@@ -33,9 +35,14 @@ func _physics_process(delta):
 	
 	# Change direction of the Directional Offset
 	if target.velocity.x > 0:
-		currentDirOffset.x = directionalOffset.x
+		facingRight = true
 	else: if target.velocity.x < 0:
-		currentDirOffset.x = -directionalOffset.x
+		facingRight = false
+		
+	if facingRight:
+		currentDirOffset.x = lerp(currentDirOffset.x, dirOffset.x, dirOffsetSpeed)
+	else:
+		currentDirOffset.x = lerp(currentDirOffset.x, -dirOffset.x, dirOffsetSpeed)
 	
 	# Set the position of the camera
 	myPosition.x = lerp(targetPosition.x, fixedOffset.x + mousePosition.x, mouseMoveAmount.x)
@@ -46,16 +53,16 @@ func _physics_process(delta):
 	
 	if usingMouse:
 		# Mouse Input
-		posX = lerp(position.x, myPosition.x + currentDirOffset.x, smoothness.x)
-		posY = lerp(position.y, myPosition.y, smoothness.y)
+		posX = lerp(position.x, myPosition.x + currentDirOffset.x, speed.x)
+		posY = lerp(position.y, myPosition.y, speed.y)
 	else:
 		# Dead Zone
 		if (joystickDirection.x < joystickDead && joystickDirection.x > -joystickDead) && (joystickDirection.y < joystickDead && joystickDirection.y > -joystickDead):
 			joystickDirection = Vector2.ZERO
 			
 		# Controller Input
-		posX = lerp(position.x, target.position.x + fixedOffset.x + currentDirOffset.x + (joystickDirection.x * joystickMoveAmount.x), smoothness.x)
-		posY = lerp(position.y, target.position.y + fixedOffset.y + (joystickDirection.y * joystickMoveAmount.y), smoothness.y)
+		posX = lerp(position.x, target.position.x + fixedOffset.x + currentDirOffset.x + (joystickDirection.x * joystickMoveAmount.x), speed.x)
+		posY = lerp(position.y, target.position.y + fixedOffset.y + (joystickDirection.y * joystickMoveAmount.y), speed.y)
 		
 	# Get Joystick Input
 	var joystickDirectionX = Input.get_axis("joystick_look_left", "joystick_look_right")
