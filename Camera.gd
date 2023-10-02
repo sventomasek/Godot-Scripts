6ba8cbda54182c4126a2cd517878cae3950441af
Sven Tomašek <61661787+sventomasek@users.noreply.github.com>
2023-10-02 18:54:58 +0200

    Improved Camera.gd's readability

--- Camera.gd
+++ Camera.gd
@@ -35,7 +35,7 @@ var shakeTimer = 0
 var shakeRecoverySpeed = 0.5
 var shakeOffset = Vector2.ZERO
 
-## The rest of the stuff
+# The rest of the stuff
 var targetPosition = Vector2.ZERO
 
 var usingMouse = true
@@ -50,6 +50,31 @@ var isIdle = true
 var cameraBoundPosition = Vector2.ZERO
 
 func _process(delta):
+	GetPosition()
+	CenterOnIdle(delta)
+	MouseInWindow()
+	TargetDirection()
+	ControllerInput()
+	Screenshake(delta)
+
+func _physics_process(delta):
+	DirectionalOffset()
+	ApplyPosition()
+
+func _input(event):
+	# Switch to Mouse Input
+	if event is InputEventMouseButton:
+		usingMouse = true
+
+func _notification(event):
+	# Detect when Mouse is inside/outside the Game's Window
+	match event:
+		NOTIFICATION_WM_MOUSE_EXIT:
+			mouseInWindow = false
+		NOTIFICATION_WM_MOUSE_ENTER:
+			mouseInWindow = true
+
+func GetPosition():
 	# Find the place where the Camera should be, based on Target's position
 	if (!boundsMovement):
 		targetPosition = target.position
@@ -67,7 +92,8 @@ func _process(delta):
 			cameraBoundPosition.y -= boundsPosition.y + fixedOffset.y
 			
 		targetPosition.y = cameraBoundPosition.y
-	
+
+func CenterOnIdle(delta):
 	# Center the Camera when Idle
 	idleDelayValue -= delta
 	
@@ -77,13 +103,15 @@ func _process(delta):
 		isIdle = false
 		
 	if target.velocity != Vector2.ZERO: idleDelayValue = idleDelay
-	
+
+func MouseInWindow():
 	# Check if the Mouse is inside the Game's Window
 	if checkIfMouseInWindow:
 		if mouseInWindow: mousePosition = get_global_mouse_position()
 	else:
 		mousePosition = get_global_mouse_position()
-		
+
+func TargetDirection():
 	# Check where the target is facing
 	if target.velocity.x > 0:
 		facingRight = true
@@ -94,7 +122,8 @@ func _process(delta):
 		facingUp = true
 	elif target.velocity.y > 0:
 		facingUp = false
-		
+
+func ControllerInput():
 	# Get Joystick Input
 	var joystickDirectionX = Input.get_axis("joystick_look_left", "joystick_look_right")
 	var joystickDirectionY = Input.get_axis("joystick_look_down", "joystick_look_up")
@@ -103,10 +132,9 @@ func _process(delta):
 	# Switch to Controller Input
 	if (joystickDirection.x > joystickDead || joystickDirection.x < -joystickDead) || (joystickDirection.y > joystickDead || joystickDirection.y < -joystickDead):
 		usingMouse = false
-		
+
+func Screenshake(delta):
 	# Screenshake
-	#if Input.is_key_pressed(KEY_F1): screenshake(0.2, 10, 0.01)
-	
 	if shakeTimer > 0:
 		var random = Vector2(randf_range(-shakeStrength, shakeStrength), randf_range(-shakeStrength, shakeStrength))
 		shakeOffset = lerp(shakeOffset, random, shakeSpeed)
@@ -114,9 +142,14 @@ func _process(delta):
 	else:
 		shakeOffset = lerp(shakeOffset, Vector2.ZERO, shakeRecoverySpeed)
 
-func _physics_process(delta):
-	var myPosition = Vector2(0, 0)
-	
+	#if Input.is_key_pressed(KEY_F1): StartScreenshake(0.2, 10, 0.01)
+
+func StartScreenshake(time: float, strength: float, speed: float):
+	shakeStrength = strength
+	shakeSpeed = speed
+	shakeTimer = time
+
+func DirectionalOffset():
 	# Change direction of the Directional Offset
 	if centerOnIdle && isIdle:
 		currentDirOffset.x = lerp(currentDirOffset.x, Vector2.ZERO.x, dirOffsetSpeed.x)
@@ -131,14 +164,17 @@ func _physics_process(delta):
 		currentDirOffset.y = lerp(currentDirOffset.y, dirOffset.y, dirOffsetSpeed.y)
 	else:
 		currentDirOffset.y = lerp(currentDirOffset.y, -dirOffset.y, dirOffsetSpeed.y)
-	
+
+func ApplyPosition():
 	# Set the position of the camera
+	var myPosition = Vector2(0, 0)
+
 	myPosition.x = lerp(targetPosition.x, mousePosition.x, mouseMoveAmount.x)
 	myPosition.y = lerp(targetPosition.y, mousePosition.y, mouseMoveAmount.y)
-	
+
 	var posX = 0
 	var posY = 0
-	
+
 	if usingMouse:
 		# Mouse Input
 		posX = lerp(position.x, myPosition.x + fixedOffset.x + currentDirOffset.x + shakeOffset.x, speed.x)
@@ -155,21 +191,3 @@ func _physics_process(delta):
 	# Apply the Camera's Position
 	if !ignoreX: position.x = posX
 	if !ignoreY: position.y = posY
-
-func _input(event):
-	# Switch to Mouse Input
-	if event is InputEventMouseButton:
-		usingMouse = true
-
-func _notification(event):
-	# Detect when Mouse is inside/outside the Game's Window
-	match event:
-		NOTIFICATION_WM_MOUSE_EXIT:
-			mouseInWindow = false
-		NOTIFICATION_WM_MOUSE_ENTER:
-			mouseInWindow = true
-
-func screenshake(time: float, strength: float, speed: float):
-	shakeStrength = strength
-	shakeSpeed = speed
-	shakeTimer = time
